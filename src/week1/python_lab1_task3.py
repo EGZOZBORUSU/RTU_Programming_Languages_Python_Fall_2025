e34c2b2887f72828bb5ce8b333b576e7cb3ab020
Zeron <84472661+EGZOZBORUSU@users.noreply.github.com>
2025-11-02 16:48:16 +0200

    Update python_lab1_task3.py

--- src/week1/python_lab1_task3.py
+++ src/week1/python_lab1_task3.py
@@ -9,10 +9,27 @@ Return results as a tuple and print summary in main.
 """
 
 def analyze_sentence(text):
-    """Return length, word count, and whether 'Python' appears in text."""
-    # TODO: implement function logic
-    pass
+    count_chars = len(text)
+
+    words_list = text.split()
+    count_words = len(words_list)
+
+    has_python = 'python' in text.lower()
+
+    return (count_chars, count_words, has_python)
 
 if __name__ == "__main__":
-    # TODO: read sentence from input, call function, and print results
-    pass
+    print("Sentence Analizer Tool!")
+
+    user_sentense = input("Pls type a sentense to analize: ")
+
+    results_tuple = analyze_sentence(user_sentense)
+
+    char_cunt = results_tuple[0]
+    word_cunt = results_tuple[1]
+    has_py = results_tuple[2]
+
+    print("\nAnaliz is complete:")
+    print(f"Total charecters (with spaces): {char_cunt}")
+    print(f"Number of words: {word_cunt}")
+    print(f"Dous it have the word 'Python'?: {has_py}")
