007af5cdfef499f49f426e4bdbba264522a81df2
Zeron <84472661+EGZOZBORUSU@users.noreply.github.com>
2025-11-02 16:46:58 +0200

    Update python_lab1_task1.py

--- src/week1/python_lab1_task1.py
+++ src/week1/python_lab1_task1.py
@@ -7,13 +7,21 @@ Use the math module for π.
 Ask user for radius and print result with 2 decimals.
 """
 
-# TODO: import math
+import math
 
 def circle_area(radius):
     """Return the area of a circle given its radius."""
-    # TODO: implement formula using math.pi
-    pass
+    area = math.pi * (radius * radius)
+    return area
 
 if __name__ == "__main__":
-    # TODO: ask for user input, call circle_area(), and print formatted result
-    pass
+    print("circle Area Calculater!")
+
+    user_input_radius = input("please enter the radiyus of the circle: ")
+
+    radius_num = float(user_input_radius)
+
+    result_area = circle_area(radius_num)
+
+    print(f"The area for radiyus {radius_num} is: {result_area:.2f}")
+
